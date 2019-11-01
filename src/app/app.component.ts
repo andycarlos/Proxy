0ffcc9500b557ca32d8b4e41504506331a9bcd57
acalfonso <acalfonso@WMIA0564>
2019-11-01 09:12:30 -0400

    My commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proxy';
+  constructor(private http:HttpClient)
+  {
+    const body = `<?xml version="1.0" encoding="utf-8"?>
+    <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+      <soap:Body>
+        <getTracking xmlns="http://tempuri.org/">
+          <pAwbno>57650272784</pAwbno>
+        </getTracking>
+      </soap:Body>
+    </soap:Envelope>`;
+    let head : HttpHeaders = new HttpHeaders().set("Content-Type","text/xml");
+    http.post('/api/wsutil.asmx',body,{responseType:'text', headers:head}).subscribe(resul=>console.log(resul));  
+  }
 }
